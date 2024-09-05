016ce4a4a73161ddf68c5d8d5486686ba28b7d48
osakanatomozo <0yj3251k2p70v9f@i.softbank.jp>
2024-09-05 23:07:44 +0900

    third-commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     Public function index() {
-        return view('posts.index');
+        $posts = DB::table('posts')->get();
+        return view('posts.index', compact('posts'));
     }
 }
